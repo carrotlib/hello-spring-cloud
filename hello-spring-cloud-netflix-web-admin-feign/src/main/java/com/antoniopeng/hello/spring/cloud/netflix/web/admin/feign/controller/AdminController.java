40fc5f86bc2c07469ed152dc8afc6e1b94acc326
AntonioPeng <antoniopeng@foxmail.com>
2019-09-15 23:17:40 +0800

    init commit

---
+++ hello-spring-cloud-netflix-web-admin-feign/src/main/java/com/antoniopeng/hello/spring/cloud/netflix/web/admin/feign/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.antoniopeng.hello.spring.cloud.netflix.web.admin.feign.controller;
+
+import com.antoniopeng.hello.spring.cloud.netflix.web.admin.feign.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @RequestMapping(value = "hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String message) {
+        return adminService.sayHi(message);
+    }
+
+}
